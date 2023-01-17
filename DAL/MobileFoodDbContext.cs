168c0533a6be58698e2869277d495faa4e93a255
Zathael <zathaelV@gmail.com>
2023-01-17 11:17:53 -0800

    backend setup

---
+++ DAL/MobileFoodDbContext.cs
@@ -0,0 +1,33 @@
+﻿using CsvHelper;
+using FoodFacilities.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+
+namespace FoodFacilities.Api.DAL
+{
+    /// <summary>
+    /// DBContext for MobileFoodFacilities
+    /// </summary>
+    public class MobileFoodDbContext : DbContext
+    {
+        public DbSet<MobileFoodFacility> mobileFoodFacilities { get; set; }
+
+        /// <summary>
+        /// Loads the MobileFoodFacility data from the CSV file
+        /// dev note: overrides OnModelCreating to use an in-memory database
+        /// dev note 2: remember to use usings!
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            using (var reader = new StreamReader("DAL\\MobileFoodFacility.csv"))
+            {
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    var records = csv.GetRecords<MobileFoodFacility>();
+                    modelBuilder.Entity<MobileFoodFacility>().HasData(records);
+                }
+            }
+        }
+    }
+}
--- Models/MobileFoodFacility.cs
+++ Models/MobileFoodFacility.cs
@@ -11,50 +11,126 @@
         /// </summary>
         public int LocationId { get; set; }
         /// <summary>
-        /// Applicant
+        /// Applicant Name
         /// </summary>
         public string Applicant { get; set; }
         /// <summary>
-        /// 
+        /// Facility Type (Truck, Push Cart, etc.)
         /// </summary>
         public string FacilityType { get; set; }
         /// <summary>
-        /// 
+        /// Cnn Id
         /// </summary>
         public int Cnn { get; set; }
         /// <summary>
-        /// 
+        /// Location's Description
         /// </summary>
         public string LocationDescription { get; set; }
         /// <summary>
-        /// 
+        /// Address
         /// </summary>
         public string Address { get; set; }
         /// <summary>
-        /// 
+        /// Blocklot Id
         /// </summary>
         public int BlockLot { get; set; }
-        public string Block { get; set; }
-        public string Lot { get; set; }
+        /// <summary>
+        /// Block Id
+        /// </summary>
+        public int Block { get; set; }
+        /// <summary>
+        /// Lot Id
+        /// </summary>
+        public int Lot { get; set; }
+        /// <summary>
+        /// Permit 
+        /// </summary>
         public string Permit { get; set; }
+        /// <summary>
+        /// Status Message
+        /// </summary>
         public string Status { get; set; }
+        /// <summary>
+        /// Descriptive List of Items as a string
+        /// </summary>
         public string FoodItems { get; set; }
-        public string X { get; set; }
-        public string Y { get; set; }
-        public string Latitude { get; set; }
-        public string Longitude { get; set; }
+        /// <summary>
+        /// X position, sometimes null
+        /// </summary>
+        public int? X { get; set; }
+        /// <summary>
+        /// Y position, sometimes null
+        /// </summary>
+        public int? Y { get; set; }
+        /// <summary>
+        /// Latitude
+        /// </summary>
+        public int Latitude { get; set; }
+        /// <summary>
+        /// Longitude
+        /// </summary>
+        public int Longitude { get; set; }
+        /// <summary>
+        /// Schedule as a url
+        /// </summary>
         public string Schedule { get; set; }
+        /// <summary>
+        /// Days & Hours of Vendor (non-useful format)
+        /// </summary>
         public string Dayshours { get; set; }
+        /// <summary>
+        /// NOISent 
+        /// dev note: all columns null. Could be a Boolean flag that "yes we sent a NOI" or be more complex. set as string
+        /// </summary>
         public string NOISent { get; set; }
+        /// <summary>
+        /// Approved Time
+        /// dev note: kept as string - we are given no UTC or timezone. We 'could' assume PST and convert UTC from there. 
+        ///           Or we could keep data as data, and interpret it when we utilize it.
+        /// </summary>
         public string Approved { get; set; }
-        public string Received { get; set; }
-        public string PriorPermit { get; set; }
+        /// <summary>
+        /// Received Id
+        /// </summary>
+        public int Received { get; set; }
+        /// <summary>
+        /// PriorPermit
+        /// </summary>
+        public bool PriorPermit { get; set; }
+        /// <summary>
+        /// Expiration Time
+        /// dev note: kept as string - we are given no UTC or timezone. We 'could' assume PST and convert UTC from there. 
+        ///           Or we could keep data as data, and interpret it when we utilize it.
+        /// </summary>
         public string ExpirationDate { get; set; }
+        /// <summary>
+        /// Location 
+        /// </summary>
         public string Location { get; set; }
-        public string FirePreventionDistricts { get; set; }
-        public string PoliceDistricts { get; set; }
-        public string SupervisorDistricts { get; set; }
-        public string ZipCodes { get; set; }
-        public string NeighborhoodsOld { get; set; }
+        /// <summary>
+        /// FirePreventionDistricts Id?
+        /// dev note: Could be a quantity, but is an occasionall null, singular unsigned number
+        /// </summary>
+        public int? FirePreventionDistricts { get; set; }
+        /// <summary>
+        /// PoliceDistricts Id?
+        /// dev note: Could be a quantity, but is an occasionall null, singular unsigned number
+        /// </summary>
+        public int? PoliceDistricts { get; set; }
+        /// <summary>
+        /// SupervisorDistricts Id?
+        /// dev note: Could be a quantity, but is an occasionall null, singular unsigned number
+        /// </summary>
+        public int? SupervisorDistricts { get; set; }
+        /// <summary>
+        /// Zipcode
+        /// dev note: Why is this plural when we are expecting a single value?
+        /// </summary>
+        public int? ZipCodes { get; set; }
+        /// <summary>
+        /// NeighborhoodsOld Id
+        /// 
+        /// </summary>
+        public int? NeighborhoodsOld { get; set; }
     }
 }
